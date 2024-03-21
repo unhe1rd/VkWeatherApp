e34230594ffbaad9b8014ef5918d2a8045cccdc6
Mikhail Ulanov <mixaulanoff@yandex.ru>
2024-03-21 23:19:53 +0300

    create project

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Mike Ulanov on 21.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
